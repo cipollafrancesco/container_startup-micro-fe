56544771cf7f38440452f20ce9304e2cc30df40c
cipollafrancesco <francescocpll@gmail.com>
2021-01-13 13:54:08 +0100

    Fixed material-ui classNames collisions between Applications

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import {BrowserRouter} from 'react-router-dom'
+import {createGenerateClassName, StylesProvider} from '@material-ui/core'
+
+// AVOID classNames Collisions
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'container'
+})
 
 export default () => {
     return (
-        <BrowserRouter>
-            <>
-                {/* CONTAINER COMPONENT HEADER*/}
-                <Header/>
-                {/* MARKETING APP (contains Marketing Micro-FE)*/}
-                <MarketingApp/>
-            </>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <>
+                    {/* CONTAINER COMPONENT HEADER*/}
+                    <Header/>
+                    {/* MARKETING APP (contains Marketing Micro-FE)*/}
+                    <MarketingApp/>
+                </>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
